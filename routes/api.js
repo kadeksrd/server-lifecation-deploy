971881a52fbaec0df42f6ed79e0c17d7f8e19924
kadek srd <kadeksuryard13@gmail.com>
2023-07-18 19:14:15 +0700

    fix cors

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,5 @@ const cors = require("cors");
 
 router.get("/landing-page", cors(), apiController.landingPage);
 router.get("/detail-page/:id", cors(), apiController.detailPage);
-router.post(
-  "/booking-page",
-  cors(corsOption),
-  uploadSingle,
-  apiController.bookingPage
-);
+router.post("/booking-page", cors(), uploadSingle, apiController.bookingPage);
 module.exports = router;
